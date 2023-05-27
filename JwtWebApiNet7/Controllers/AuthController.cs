8f9186a31f6dbed7dbadec4b34243feeac6e0705
minh1304 <vtm1304@gmail.com>
2023-05-27 17:19:42 +0700

    Add project files.

---
+++ JwtWebApiNet7/Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿using JwtWebApiNet7.DTOs;
+using JwtWebApiNet7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtWebApiNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _secretKey;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:Token"]));
+
+        }
+
+        // Register
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDTO request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            if (user.UserName == request.UserName)
+            {
+                return BadRequest("Tên người dùng đã tồn tại");
+            }
+            user.UserName = request.UserName;
+            user.PasswordHash = passwordHash;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDTO request)
+        {
+            if (user.UserName != request.UserName)
+            {
+                return BadRequest("Sai tên đăng nhập");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Sai mật khẩu");
+            }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var tokenOptions = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: new SigningCredentials(_secretKey, SecurityAlgorithms.HmacSha512)
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return tokenString;
+        }
+    }
+}
---
+++ JwtWebApiNet7/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtWebApiNet7.DTOs
+{
+    public class UserDTO
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
